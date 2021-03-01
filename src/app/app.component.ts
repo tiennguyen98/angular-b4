f7596e885e03adf882b1355bf1e8b97d358ea5e5
Tien, Nguyen Van <tiennv@biplus.com.vn>
2021-03-01 21:42:19 +0700

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo-angular-b4';
+  value;
+  result;
+  number1: number;
+  number2: number;
+
+  getValue(event: any): void {
+    this.value = event.target.value;
+  }
+
+  calculator(): any {
+    if (this.value === '+'){
+      this.result = this.number1 + this.number2;
+    }
+    if (this.value === '-'){
+      this.result = this.number1 - this.number2;
+    }
+    if (this.value === '*'){
+      this.result = this.number1 * this.number2;
+    }
+    if (this.value === '/'){
+      this.result = this.number1 / this.number2;
+    }
+  }
 }
