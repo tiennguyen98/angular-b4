9096708b8052ab0a9f5160519923717b79b050a3
Tien, Nguyen Van <tiennv@biplus.com.vn>
2021-03-04 12:28:03 +0700

    Fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Observable, timer} from 'rxjs';
+import {map, take} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -6,28 +9,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo-angular-b4';
-  value;
-  result;
-  number1: number;
-  number2: number;
+  title = 'untitled';
+  listBackgroundChoose = [];
+  private intervalId = 0;
+  count = 5;
+  remainingTime = this.count;
+  backgroundShow = '';
+  backgroundChoose = '#000000';
+  isStart: boolean;
 
-  getValue(event: any): void {
-    this.value = event.target.value;
+  constructor() {
+  }
+  onChange(): void {
+    this.listBackgroundChoose.push(this.backgroundChoose);
+    if (this.backgroundShow === '') this.backgroundShow = this.listBackgroundChoose[0];
   }
 
-  calculator(): any {
-    if (this.value === '+'){
-      this.result = this.number1 + this.number2;
+  clearTimer(): void {
+    clearInterval(this.intervalId);
+  }
+
+
+  start(): void {
+    this.isStart = true;
+    this.countDown();
+    if (this.remainingTime <= 0) {
+      this.remainingTime = this.count;
     }
-    if (this.value === '-'){
-      this.result = this.number1 - this.number2;
-    }
-    if (this.value === '*'){
-      this.result = this.number1 * this.number2;
-    }
-    if (this.value === '/'){
-      this.result = this.number1 / this.number2;
-    }
+  }
+  reset(): void {
+    this.isStart = false;
+    this.clearTimer();
+    this.remainingTime = this.count;
+    this.index = 0;
+    this.backgroundShow = this.listBackgroundChoose[0];
+  }
+  index = 0;
+  private countDown(): void {
+    this.clearTimer();
+    this.intervalId = window.setInterval(() => {
+      this.remainingTime -= 1;
+      if (this.remainingTime === 0) {
+        this.remainingTime = this.count;
+        if (this.index === this.listBackgroundChoose.length - 1) {
+          this.index = 0;
+        } else {
+          this.index++;
+        }
+        this.backgroundShow = this.listBackgroundChoose[this.index];
+
+      }
+    }, 1000);
   }
 }
